d727672501c5efea95ad002e94c76fbb42eb49c4
Admin <kyawsoelinn24@gmail.com>
2025-12-16 13:03:35 +0630

    setup toolbar

---
+++ app/src/main/java/com/example/drawerlayout/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.drawerlayout
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.dlayout)
+        navView = findViewById(R.id.naviView)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.profile -> {
+                    Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
+                }
+                R.id.newgroup -> {
+                    Toast.makeText(this, "New Group", Toast.LENGTH_SHORT).show()
+                }
+        }
+        true
+        }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (toggle.onOptionsItemSelected(item)) {
+            true
+        } else{
+            super.onOptionsItemSelected(item)
+        }
+    }
+}
